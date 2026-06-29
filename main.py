f210d19f2910cf80fce5124bf2931a10a7fb891f
BaxMaier <max.baier92@googlemail.com>
2026-06-29 12:53:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-
-from telegram import Bot
 import os
+import asyncio
+from telegram import Bot
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-bot = Bot(token=TOKEN)
-bot.send_message(chat_id=CHAT_ID, text="✅ TEST: Telegram funktioniert!")
+
+async def send_test():
+    bot = Bot(token=TOKEN)
+    await bot.send_message(
+        chat_id=CHAT_ID,
+        text="✅ TEST: Telegram funktioniert jetzt!"
+    )
+
+
+if __name__ == "__main__":
+    asyncio.run(send_test())
