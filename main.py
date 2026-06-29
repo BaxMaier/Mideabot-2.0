6d3235c9ad20ed20720fa430ea0ae0584f9fa192
BaxMaier <max.baier92@googlemail.com>
2026-06-29 12:48:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-
 from telegram import Bot
 import os
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-print("TOKEN:", TOKEN)
-print("CHAT_ID:", CHAT_ID)
-
 bot = Bot(token=TOKEN)
-bot.send_message(chat_id=CHAT_ID, text="✅ TEST: Telegram funktioniert!")
+bot.send_message(chat_id=CHAT_ID, text="✅ Telegram Bot funktioniert!")
+``
